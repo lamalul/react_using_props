4d40a1842ec35d755ebc1171183f6345e957215b
“Lamaosman2012” <lamaosman@yahoo.de>
2023-12-09 19:53:19 +0100

    Before eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product'
+// import { Component } from 'react';
+import productsData from process.env.PUBLIC_URL + '/data.json'; 
+/* function App() {
+  return (
+    <div className="App">
+      <Product
+  img="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/82/6142201/1.jpg?2933"
+  name="Cyxus"
+  desc="Non-Slip Fitness Leisure Running Sneakers"
+  price="$29"
+/>
+    </div>
+  );
+} */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {productsData.map((product, index) => (
+        <Product
+          key={index}
+          img={product.img}
+          name={product.name}
+          desc={product.desc}
+          price={product.price}
+        />
+      ))}
     </div>
   );
 }
+
 
 export default App;
